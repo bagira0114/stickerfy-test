fb65bcb3704a3fdaf39ede08754c338723e22cec
Orsolya Berger-Kéry <keryorsi@gmail.com>
2024-02-05 15:10:10 +0100

    Shows the cart when one single sad item added

--- cypress/e2e/view_cart_test.spec.cy.js
+++ cypress/e2e/view_cart_test.spec.cy.js
@@ -57,4 +57,30 @@ describe('View cart', () => {
 
 
     })
+
+    it('Shows the cart in case of one single added sad item', () => {
+        cy.visit('http://localhost:3000/');
+        cy.get('body > div > div:nth-child(2) > div:nth-child(3) > div > div > div > a').click();
+        cy.get('#cart').click();
+        cy.url().should('include', '/shopping-cart/');
+        cy.get('.list-group-item > strong').should('have.text', 'Sad');
+        cy.get('.list-group-item > .badge').should('have.text', '1');
+        cy.get('.col-sm-6 > strong')
+            .invoke('text')
+            .invoke('slice', '7')
+            .then(amount => {
+
+
+                cy.get('.label')
+                    .invoke('text')
+                    .invoke('slice', '1')
+                    .then(amount2 => {
+
+                        expect(amount.trim()).to.equal(amount2.trim());
+
+                    })
+            })
+
+
+    })
 })
