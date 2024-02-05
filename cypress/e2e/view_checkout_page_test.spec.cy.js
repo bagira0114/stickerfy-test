edd69bbf218907eb6994facf81e84477050c1e2a
Orsolya Berger-Kéry <keryorsi@gmail.com>
2024-02-05 15:46:26 +0100

    Shows the checkout page in case of one single added angry item

--- cypress/e2e/view_checkout_page_test.spec.cy.js
+++ cypress/e2e/view_checkout_page_test.spec.cy.js
@@ -17,4 +17,24 @@ it('Shows the checkout page in case of one single added happy item', () => {
     })
     })
 })
+
+it('Shows the checkout page in case of one single added angry item', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('body > div > div:nth-child(2) > div:nth-child(2) > div > div > div > a').click();
+    cy.get('#cart').click();
+    cy.get('.col-sm-6 > strong')
+    .invoke('text')
+    .invoke('slice', '7')
+    .then(amount => {
+    cy.get('#checkout').click();
+    cy.url().should('include', '/checkout');
+    cy.get('#total')
+    .invoke('text')
+    .invoke('slice', '8')
+    .then(amount2 => {
+        expect(amount.trim()).to.equal(amount2.trim());
+    })
+    })
+})
+
 })
