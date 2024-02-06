b80a102a485c209ce33d8e014421fd62fb85a2ed
Orsolya Berger-Kéry <keryorsi@gmail.com>
2024-02-06 10:00:01 +0100

    Adds multiple same angry or sad items to the cart

--- cypress/e2e/add_multiple_items_test.spec.cy.js
+++ cypress/e2e/add_multiple_items_test.spec.cy.js
@@ -5,4 +5,16 @@ describe('Add multiple items to the cart', () =>{
         cy.get('body > div > div:nth-child(2) > div:nth-child(1) > div > div > div > a').click();
         cy.get('.badge').should('have.text', 2);
     })
+    it('Adds multiple same angry items to the cart', () => {
+        cy.visit('http://localhost:3000/');
+        cy.get(':nth-child(2) > .thumbnail > .caption > .clearfix > .btn').click();
+        cy.get(':nth-child(2) > .thumbnail > .caption > .clearfix > .btn').click();
+        cy.get('.badge').should('have.text', 2);
+    })
+    it('Adds multiple same sad items to the cart', () => {
+        cy.visit('http://localhost:3000/');
+        cy.get(':nth-child(3) > .thumbnail > .caption > .clearfix > .btn').click();
+        cy.get(':nth-child(3) > .thumbnail > .caption > .clearfix > .btn').click();
+        cy.get('.badge').should('have.text', 2);
+    })
 })
