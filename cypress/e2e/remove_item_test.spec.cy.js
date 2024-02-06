e4355e63307d06c3a3744e148e4f97cf4d98d1fa
Orsolya Berger-Kéry <keryorsi@gmail.com>
2024-02-06 11:15:03 +0100

    Removes all angry/sad items in case of multiple added angry/sad items

--- cypress/e2e/remove_item_test.spec.cy.js
+++ cypress/e2e/remove_item_test.spec.cy.js
@@ -73,4 +73,28 @@ describe('Remove items from cart', () => {
 
     })
 
+    it('Removes all angry items in case of multiple added angry items', () => {
+        cy.visit('http://localhost:3000/');
+        cy.get('body > div > div:nth-child(2) > div:nth-child(2) > div > div > div > a').click();
+        cy.get('body > div > div:nth-child(2) > div:nth-child(2) > div > div > div > a').click();
+        cy.get('#cart').click();
+        cy.get('.list-group-item > .badge').should('have.text', '2');
+        cy.get('#products > ul > li > div > button').click();
+        cy.get('#products > ul > li > div > ul > li:nth-child(2) > a').click();
+        cy.get('#checkout').should('not.exist');
+
+    })
+
+    it('Removes all sad items in case of multiple added sad items', () => {
+        cy.visit('http://localhost:3000/');
+        cy.get('body > div > div:nth-child(2) > div:nth-child(3) > div > div > div > a').click();
+        cy.get('body > div > div:nth-child(2) > div:nth-child(3) > div > div > div > a').click();
+        cy.get('#cart').click();
+        cy.get('.list-group-item > .badge').should('have.text', '2');
+        cy.get('#products > ul > li > div > button').click();
+        cy.get('#products > ul > li > div > ul > li:nth-child(2) > a').click();
+        cy.get('#checkout').should('not.exist');
+
+    })
+
 })
