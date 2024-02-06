4c08424c2668001d58fa0a8503b2444bdb309a05
Orsolya Berger-Kéry <keryorsi@gmail.com>
2024-02-06 13:48:52 +0100

    Shows the cart in case of multiple added different items and some refactoring

--- cypress/e2e/view_cart_test.spec.cy.js
+++ cypress/e2e/view_cart_test.spec.cy.js
@@ -1,15 +1,20 @@
+const HAPPY_ITEM = 'body > div > div:nth-child(2) > div:nth-child(1) > div > div > div > a';
+const ANGRY_ITEM = 'body > div > div:nth-child(2) > div:nth-child(2) > div > div > div > a';
+const SAD_ITEM = 'body > div > div:nth-child(2) > div:nth-child(3) > div > div > div > a';
+const CART = '#cart';
+const CHECKOUT = '#checkout';
 describe('View cart', () => {
     it('Shows the cart when no added items', () => {
         cy.visit('http://localhost:3000/');
-        cy.get('#cart').click();
+        cy.get(CART).click();
         cy.url().should('include', '/shopping-cart/');
-        cy.get('#checkout').should('not.exist');
+        cy.get(CHECKOUT).should('not.exist');
     })
 
     it('Shows the cart in case of one single added happy item', () => {
         cy.visit('http://localhost:3000/');
-        cy.get('body > div > div:nth-child(2) > div:nth-child(1) > div > div > div > a').click();
-        cy.get('#cart').click();
+        cy.get(HAPPY_ITEM).click();
+        cy.get(CART).click();
         cy.url().should('include', '/shopping-cart/');
         cy.get('.list-group-item > strong').should('have.text', 'Happy');
         cy.get('.list-group-item > .badge').should('have.text', '1');
@@ -34,8 +39,8 @@ describe('View cart', () => {
 
     it('Shows the cart in case of one single added angry item', () => {
         cy.visit('http://localhost:3000/');
-        cy.get('body > div > div:nth-child(2) > div:nth-child(2) > div > div > div > a').click();
-        cy.get('#cart').click();
+        cy.get(ANGRY_ITEM).click();
+        cy.get(CART).click();
         cy.url().should('include', '/shopping-cart/');
         cy.get('.list-group-item > strong').should('have.text', 'Angry');
         cy.get('.list-group-item > .badge').should('have.text', '1');
@@ -60,8 +65,8 @@ describe('View cart', () => {
 
     it('Shows the cart in case of one single added sad item', () => {
         cy.visit('http://localhost:3000/');
-        cy.get('body > div > div:nth-child(2) > div:nth-child(3) > div > div > div > a').click();
-        cy.get('#cart').click();
+        cy.get(SAD_ITEM).click();
+        cy.get(CART).click();
         cy.url().should('include', '/shopping-cart/');
         cy.get('.list-group-item > strong').should('have.text', 'Sad');
         cy.get('.list-group-item > .badge').should('have.text', '1');
@@ -86,9 +91,9 @@ describe('View cart', () => {
 
     it('Shows the cart in case of multiple added happy items', () => {
         cy.visit('http://localhost:3000/');
-        cy.get('body > div > div:nth-child(2) > div:nth-child(1) > div > div > div > a').click();
-        cy.get('body > div > div:nth-child(2) > div:nth-child(1) > div > div > div > a').click();
-        cy.get('#cart').click();
+        cy.get(HAPPY_ITEM).click();
+        cy.get(HAPPY_ITEM).click();
+        cy.get(CART).click();
         cy.get('.list-group-item > strong').should('have.text', 'Happy');
         cy.get('.list-group-item > .badge').should('have.text', '2');
         cy.get('.col-sm-6 > strong')
@@ -110,9 +115,9 @@ describe('View cart', () => {
 
     it('Shows the cart in case of multiple added angry items', () => {
         cy.visit('http://localhost:3000/');
-        cy.get('body > div > div:nth-child(2) > div:nth-child(2) > div > div > div > a').click();
-        cy.get('body > div > div:nth-child(2) > div:nth-child(2) > div > div > div > a').click();
-        cy.get('#cart').click();
+        cy.get(ANGRY_ITEM).click();
+        cy.get(ANGRY_ITEM).click();
+        cy.get(CART).click();
         cy.get('.list-group-item > strong').should('have.text', 'Angry');
         cy.get('.list-group-item > .badge').should('have.text', '2');
         cy.get('.col-sm-6 > strong')
@@ -134,9 +139,9 @@ describe('View cart', () => {
 
     it('Shows the cart in case of multiple added sad items', () => {
         cy.visit('http://localhost:3000/');
-        cy.get('body > div > div:nth-child(2) > div:nth-child(3) > div > div > div > a').click();
-        cy.get('body > div > div:nth-child(2) > div:nth-child(3) > div > div > div > a').click();
-        cy.get('#cart').click();
+        cy.get(SAD_ITEM).click();
+        cy.get(SAD_ITEM).click();
+        cy.get(CART).click();
         cy.get('.list-group-item > strong').should('have.text', 'Sad');
         cy.get('.list-group-item > .badge').should('have.text', '2');
         cy.get('.col-sm-6 > strong')
@@ -152,6 +157,54 @@ describe('View cart', () => {
 
                         expect(amount.trim()).to.equal(amount2.trim());
 
+                    })
+            })
+    })
+    it('Shows the cart in case of multiple added different items', () => {
+        cy.visit('http://localhost:3000/');
+        cy.get(HAPPY_ITEM).click();
+        cy.get(HAPPY_ITEM).click();
+        cy.get(ANGRY_ITEM).click();
+        cy.get(ANGRY_ITEM).click();
+        cy.get(SAD_ITEM).click();
+        cy.get(SAD_ITEM).click();
+        cy.get(CART).click();
+        cy.get('.list-group > :nth-child(1) > strong').should('have.text', 'Happy');
+        cy.get('.list-group > :nth-child(1) > .badge').should('have.text', '2');
+        cy.get('.list-group > :nth-child(2) > strong').should('have.text', 'Angry');
+        cy.get('.list-group > :nth-child(2) > .badge').should('have.text', '2');
+        cy.get('.list-group > :nth-child(3) > strong').should('have.text', 'Sad');
+        cy.get('.list-group > :nth-child(3) > .badge').should('have.text', '2');
+        cy.get(':nth-child(1) > .label')
+            .invoke('text')
+            .invoke('slice', '1')
+            .then(amount => {
+                const happyAmount = Number(amount);
+
+                cy.get(':nth-child(2) > .label')
+                    .invoke('text')
+                    .invoke('slice', '1')
+                    .then(amount2 => {
+                        const angryAmount = Number(amount2);
+
+                        cy.get(':nth-child(3) > .label')
+                            .invoke('text')
+                            .invoke('slice', '1')
+                            .then(amount3 => {
+                                const sadAmount = Number(amount3);
+
+                                cy.get('.col-sm-6 > strong')
+                                    .invoke('text')
+                                    .invoke('slice', '7')
+                                    .then(amountSum => {
+                                    
+
+                                        const sumOfAllItems = happyAmount + angryAmount + sadAmount;
+
+                                        expect(Number(amountSum.trim())).to.equal(sumOfAllItems);
+
+                                    })
+                            })
                     })
             })
     })
