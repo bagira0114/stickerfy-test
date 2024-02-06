7dcf9a3c30311ba26429405f78791bb1ba8928b7
Orsolya Berger-Kéry <keryorsi@gmail.com>
2024-02-06 09:48:03 +0100

    Removes one single sad item from cart

--- cypress/e2e/remove_item_test.spec.cy.js
+++ cypress/e2e/remove_item_test.spec.cy.js
@@ -9,9 +9,17 @@ describe('Remove items from cart', () => {
 
     })
     it('Removes one single angry item from cart', () => {
-    cy.visit('http://localhost:3000/');
-    cy.get(':nth-child(2) > .thumbnail > .caption > .clearfix > .btn').click();
-    cy.get('#cart').click();
+        cy.visit('http://localhost:3000/');
+        cy.get(':nth-child(2) > .thumbnail > .caption > .clearfix > .btn').click();
+        cy.get('#cart').click();
+        cy.get('#products > ul > li > div > button').click();
+        cy.get('#products > ul > li > div > ul > li:nth-child(1) > a').click();
+        cy.get('#checkout').should('not.exist');
+    })
+    it('Removes one single sad item from cart', () => {
+        cy.visit('http://localhost:3000/');
+        cy.get(':nth-child(3) > .thumbnail > .caption > .clearfix > .btn').click();
+        cy.get('#cart').click();
         cy.get('#products > ul > li > div > button').click();
         cy.get('#products > ul > li > div > ul > li:nth-child(1) > a').click();
         cy.get('#checkout').should('not.exist');
