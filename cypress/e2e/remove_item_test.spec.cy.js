fa7bd09cac66a67cff0c09190f3284217046e7c0
Orsolya Berger-Kéry <keryorsi@gmail.com>
2024-02-06 09:42:12 +0100

    Removes one single happy item from cart

---
+++ cypress/e2e/remove_item_test.spec.cy.js
@@ -0,0 +1,12 @@
+describe('Remove items from cart', () => {
+    it('Removes one single happy item from cart', () => {
+        cy.visit('http://localhost:3000/');
+        cy.get('body > div > div:nth-child(2) > div:nth-child(1) > div > div > div > a').click();
+        cy.get('#cart').click();
+        cy.get('#products > ul > li > div > button').click();
+        cy.get('#products > ul > li > div > ul > li:nth-child(1) > a').click();
+        cy.get('#checkout').should('not.exist');
+
+    })
+
+})
