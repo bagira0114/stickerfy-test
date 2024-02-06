fb6875232fb9c1985dfd7d588224284d333b45fe
Orsolya Berger-Kéry <keryorsi@gmail.com>
2024-02-06 10:10:02 +0100

    Shows the cart in case of multiple added sad items

--- cypress/e2e/view_cart_test.spec.cy.js
+++ cypress/e2e/view_cart_test.spec.cy.js
@@ -131,4 +131,28 @@ describe('View cart', () => {
                     })
             })
     })
+
+    it('Shows the cart in case of multiple added sad items', () => {
+        cy.visit('http://localhost:3000/');
+        cy.get('body > div > div:nth-child(2) > div:nth-child(3) > div > div > div > a').click();
+        cy.get('body > div > div:nth-child(2) > div:nth-child(3) > div > div > div > a').click();
+        cy.get('#cart').click();
+        cy.get('.list-group-item > strong').should('have.text', 'Sad');
+        cy.get('.list-group-item > .badge').should('have.text', '2');
+        cy.get('.col-sm-6 > strong')
+            .invoke('text')
+            .invoke('slice', '7')
+            .then(amount => {
+
+
+                cy.get('.label')
+                    .invoke('text')
+                    .invoke('slice', '1')
+                    .then(amount2 => {
+
+                        expect(amount.trim()).to.equal(amount2.trim());
+
+                    })
+            })
+    })
 })
