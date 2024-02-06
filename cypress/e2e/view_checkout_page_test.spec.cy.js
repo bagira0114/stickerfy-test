7240ca013bf33d10540fdcfc4c62fcb4d5cf7929
Orsolya Berger-Kéry <keryorsi@gmail.com>
2024-02-06 10:28:20 +0100

    Shows the checkout page in case of multiple added angry items

--- cypress/e2e/view_checkout_page_test.spec.cy.js
+++ cypress/e2e/view_checkout_page_test.spec.cy.js
@@ -76,4 +76,24 @@ it('Shows the checkout page in case of multiple added happy items', () => {
     })
 })
 
+it('Shows the checkout page in case of multiple added angry items', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('body > div > div:nth-child(2) > div:nth-child(2) > div > div > div > a').click();
+    cy.get('body > div > div:nth-child(2) > div:nth-child(2) > div > div > div > a').click();
+    cy.get('#cart').click();
+    cy.get('.col-sm-6 > strong')
+    .invoke('text')
+    .invoke('slice', '7')
+    .then(amount => {
+    cy.get('#checkout').click();
+    cy.url().should('include', '/checkout');
+    cy.get('#total')
+    .invoke('text')
+    .invoke('slice', '8')
+    .then(amount2 => {
+        expect(amount.trim()).to.equal(amount2.trim());
+    })
+    })
+})
+
 })
