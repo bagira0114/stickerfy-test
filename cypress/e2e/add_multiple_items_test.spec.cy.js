8ea6df6d95e6a8c759c4eb9ebd3d8a6f6089ce6d
Orsolya Berger-Kéry <keryorsi@gmail.com>
2024-02-06 13:57:31 +0100

    Refactoring

--- cypress/e2e/add_multiple_items_test.spec.cy.js
+++ cypress/e2e/add_multiple_items_test.spec.cy.js
@@ -1,27 +1,30 @@
+const HAPPY_ITEM = 'body > div > div:nth-child(2) > div:nth-child(1) > div > div > div > a';
+const ANGRY_ITEM = ':nth-child(2) > .thumbnail > .caption > .clearfix > .btn';
+const SAD_ITEM = ':nth-child(3) > .thumbnail > .caption > .clearfix > .btn';
 describe('Add multiple items to the cart', () =>{
     it('Adds multiple same happy items to the cart', () => {
         cy.visit('http://localhost:3000/');
-        cy.get('body > div > div:nth-child(2) > div:nth-child(1) > div > div > div > a').click();
-        cy.get('body > div > div:nth-child(2) > div:nth-child(1) > div > div > div > a').click();
+        cy.get(HAPPY_ITEM).click();
+        cy.get(HAPPY_ITEM).click();
         cy.get('.badge').should('have.text', 2);
     })
     it('Adds multiple same angry items to the cart', () => {
         cy.visit('http://localhost:3000/');
-        cy.get(':nth-child(2) > .thumbnail > .caption > .clearfix > .btn').click();
-        cy.get(':nth-child(2) > .thumbnail > .caption > .clearfix > .btn').click();
+        cy.get(ANGRY_ITEM).click();
+        cy.get(ANGRY_ITEM).click();
         cy.get('.badge').should('have.text', 2);
     })
     it('Adds multiple same sad items to the cart', () => {
         cy.visit('http://localhost:3000/');
-        cy.get(':nth-child(3) > .thumbnail > .caption > .clearfix > .btn').click();
-        cy.get(':nth-child(3) > .thumbnail > .caption > .clearfix > .btn').click();
+        cy.get(SAD_ITEM).click();
+        cy.get(SAD_ITEM).click();
         cy.get('.badge').should('have.text', 2);
     })
     it('Adds multiple different items to the cart', () => {
         cy.visit('http://localhost:3000/');
-        cy.get(':nth-child(1) > .thumbnail > .caption > .clearfix > .btn').click();
-        cy.get(':nth-child(2) > .thumbnail > .caption > .clearfix > .btn').click();
-        cy.get(':nth-child(3) > .thumbnail > .caption > .clearfix > .btn').click();
+        cy.get(HAPPY_ITEM).click();
+        cy.get(ANGRY_ITEM).click();
+        cy.get(SAD_ITEM).click();
         cy.get('.badge').should('have.text', 3);
     })
 })
