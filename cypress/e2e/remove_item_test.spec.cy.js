9f37f6395ad11b6e0584827e79e36b5da4b1784d
Orsolya Berger-Kéry <keryorsi@gmail.com>
2024-02-07 09:26:48 +0100

    Removes one single happy item in case of multiple different added items

--- cypress/e2e/remove_item_test.spec.cy.js
+++ cypress/e2e/remove_item_test.spec.cy.js
@@ -7,6 +7,10 @@ const OPTIONS = '#products > ul > li > div > button';
 const REMOVE_ONE = '#products > ul > li > div > ul > li:nth-child(1) > a';
 const ITEMS_NUMBER = '.list-group-item > .badge';
 const REMOVE_ALL = '#products > ul > li > div > ul > li:nth-child(2) > a';
+const LISTED_FIRST_ITEMS_NAME = '.list-group > :nth-child(1) > strong';
+const LISTED_FIRST_ITEMS_NUMBER = '.list-group > :nth-child(1) > .badge';
+const LISTED_FIRST_ITEMS_OPTIONS = ':nth-child(1) > .btn-group > .btn';
+const LISTED_FIRST_ELEMENT_REMOVE_ONE = ':nth-child(1) > .btn-group > .dropdown-menu > :nth-child(1) > a';
 describe('Remove items from cart', () => {
     it('Removes one single happy item from cart', () => {
         cy.visit('http://localhost:3000/');
@@ -104,6 +108,22 @@ describe('Remove items from cart', () => {
         cy.get(REMOVE_ALL).click();
         cy.get(CHECKOUT).should('not.exist');
 
+    })
+
+    it('Removes one single happy item in case of multiple different items', () => {
+        cy.visit('http://localhost:3000/');
+        cy.get(HAPPY_ITEM).click();
+        cy.get(HAPPY_ITEM).click();
+        cy.get(ANGRY_ITEM).click();
+        cy.get(ANGRY_ITEM).click();
+        cy.get(SAD_ITEM).click();
+        cy.get(SAD_ITEM).click();
+        cy.get(CART).click();
+        cy.get(LISTED_FIRST_ITEMS_NAME).should('have.text', 'Happy');
+        cy.get(LISTED_FIRST_ITEMS_NUMBER).should('have.text', '2');
+        cy.get(LISTED_FIRST_ITEMS_OPTIONS).click();
+        cy.get(LISTED_FIRST_ELEMENT_REMOVE_ONE).click();
+        cy.get(LISTED_FIRST_ITEMS_NUMBER).should('have.text', '1');
     })
 
 })
