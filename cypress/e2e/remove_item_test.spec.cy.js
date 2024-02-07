98f069d7b4f727303924234329e9cd94be3177a4
Orsolya Berger-Kéry <keryorsi@gmail.com>
2024-02-07 09:34:14 +0100

    Removes all happy items in case of multiple different added items

--- cypress/e2e/remove_item_test.spec.cy.js
+++ cypress/e2e/remove_item_test.spec.cy.js
@@ -10,7 +10,8 @@ const REMOVE_ALL = '#products > ul > li > div > ul > li:nth-child(2) > a';
 const LISTED_FIRST_ITEMS_NAME = '.list-group > :nth-child(1) > strong';
 const LISTED_FIRST_ITEMS_NUMBER = '.list-group > :nth-child(1) > .badge';
 const LISTED_FIRST_ITEMS_OPTIONS = ':nth-child(1) > .btn-group > .btn';
-const LISTED_FIRST_ELEMENT_REMOVE_ONE = ':nth-child(1) > .btn-group > .dropdown-menu > :nth-child(1) > a';
+const LISTED_FIRST_ITEMS_REMOVE_ONE = ':nth-child(1) > .btn-group > .dropdown-menu > :nth-child(1) > a';
+const LISTED_FIRST_ITEMS_REMOVE_ALL = ':nth-child(1) > .btn-group > .dropdown-menu > :nth-child(2) > a';
 describe('Remove items from cart', () => {
     it('Removes one single happy item from cart', () => {
         cy.visit('http://localhost:3000/');
@@ -110,7 +111,7 @@ describe('Remove items from cart', () => {
 
     })
 
-    it('Removes one single happy item in case of multiple different items', () => {
+    it('Removes one single happy item in case of multiple different added items', () => {
         cy.visit('http://localhost:3000/');
         cy.get(HAPPY_ITEM).click();
         cy.get(HAPPY_ITEM).click();
@@ -122,8 +123,24 @@ describe('Remove items from cart', () => {
         cy.get(LISTED_FIRST_ITEMS_NAME).should('have.text', 'Happy');
         cy.get(LISTED_FIRST_ITEMS_NUMBER).should('have.text', '2');
         cy.get(LISTED_FIRST_ITEMS_OPTIONS).click();
-        cy.get(LISTED_FIRST_ELEMENT_REMOVE_ONE).click();
+        cy.get(LISTED_FIRST_ITEMS_REMOVE_ONE).click();
         cy.get(LISTED_FIRST_ITEMS_NUMBER).should('have.text', '1');
+    })
+
+    it('Removes all happy items in case of multiple different added items', () => {
+        cy.visit('http://localhost:3000/');
+        cy.get(HAPPY_ITEM).click();
+        cy.get(HAPPY_ITEM).click();
+        cy.get(ANGRY_ITEM).click();
+        cy.get(ANGRY_ITEM).click();
+        cy.get(SAD_ITEM).click();
+        cy.get(SAD_ITEM).click();
+        cy.get(CART).click();
+        cy.get(LISTED_FIRST_ITEMS_NAME).should('have.text', 'Happy');
+        cy.get(LISTED_FIRST_ITEMS_NUMBER).should('have.text', '2');
+        cy.get(LISTED_FIRST_ITEMS_OPTIONS).click();
+        cy.get(LISTED_FIRST_ITEMS_REMOVE_ALL).click();
+        cy.get(LISTED_FIRST_ITEMS_NAME).should('have.text', 'Angry');
     })
 
 })
